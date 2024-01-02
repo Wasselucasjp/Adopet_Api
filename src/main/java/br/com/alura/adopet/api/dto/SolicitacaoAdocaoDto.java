6ad0a8f73277f922d8130fbbb940ad68b4291cea
Wasse Lucas <wasselucas.silva@gmail.com>
2024-01-02 17:54:55 -0300

    criando record solicitacaoAdocaoDto

--- src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDto.java
+++ src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDto.java
@@ -1,2 +1,3 @@
-package br.com.alura.adopet.api.dto;public record SolicitacaoAdocaoDto() {
-}
+package br.com.alura.adopet.api.dto;
+
+public record SolicitacaoAdocaoDto(Long idPet, Long idTutor, String motivo) {}
