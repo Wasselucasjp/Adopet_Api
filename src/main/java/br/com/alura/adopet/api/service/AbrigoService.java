c2095e5fd3a26b6402a9bac2e18c7065e9e549eb
Wasse Lucas <wasselucas.silva@gmail.com>
2024-01-04 16:59:11 -0300

    servico para listar abrigo

--- src/main/java/br/com/alura/adopet/api/service/AbrigoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AbrigoService.java
@@ -1,2 +1,28 @@
-package br.com.alura.adopet.api.service;public class AbrigoService {
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.AbrigoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.repository.AbrigoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AbrigoService {
+
+    @Autowired
+    private AbrigoRepository abrigoRepository;
+
+    public AbrigoService(AbrigoRepository abrigoRepository) {
+        this.abrigoRepository = abrigoRepository;
+    }
+
+    public List<AbrigoDto> listar(){
+       return abrigoRepository
+               .findAll()
+               .stream()
+               .map(AbrigoDto::new)
+               .toList();
+    }
 }
