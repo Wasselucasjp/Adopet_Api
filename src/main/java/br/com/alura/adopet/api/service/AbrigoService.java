814c5ffd89ef44456f81c91c1985a6cdb238d201
Wasse Lucas <wasselucas.silva@gmail.com>
2024-01-04 17:41:45 -0300

    servico para cadastro abrigo

--- src/main/java/br/com/alura/adopet/api/service/AbrigoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AbrigoService.java
@@ -1,9 +1,12 @@
 package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.AbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Abrigo;
 import br.com.alura.adopet.api.repository.AbrigoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,5 +27,13 @@ public class AbrigoService {
                .stream()
                .map(AbrigoDto::new)
                .toList();
+    }
+
+    public void cadastrar(CadastroAbrigoDto dto){
+        boolean jaCadastrado = abrigoRepository.existByNomeOrTeleFoneOrEmail(dto.nome(), dto.telefone(), dto.email());
+        if (jaCadastrado){
+            throw new ValidacaoException("Dados cadastrado em outro abrigo!");
+        }
+        abrigoRepository.save(new Abrigo(dto));
     }
 }
