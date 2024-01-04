8741c71e73527e2b1c436c4bfd7ff314475c5e8a
Wasse Lucas <wasselucas.silva@gmail.com>
2024-01-04 20:44:46 -0300

    otimizando consulta TutorRepository

--- src/main/java/br/com/alura/adopet/api/repository/TutorRepository.java
+++ src/main/java/br/com/alura/adopet/api/repository/TutorRepository.java
@@ -2,11 +2,8 @@ package br.com.alura.adopet.api.repository;
 
 import br.com.alura.adopet.api.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface TutorRepository extends JpaRepository<Tutor, Long> {
-
-    boolean existsByTelefone(String telefone);
-
-    boolean existsByEmail(String email);
-
+    boolean existsByTelefoneOrEmail(String telefone, String email);
 }
