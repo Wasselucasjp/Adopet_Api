f7dd73755d387f46a25a21613c1fa25ab3dd06eb
Wasse Lucas <wasselucas.silva@gmail.com>
2024-01-04 20:35:00 -0300

    removendo redundancia e criando contrutores para cadastrar e atulizar  tutor

--- src/main/java/br/com/alura/adopet/api/model/Tutor.java
+++ src/main/java/br/com/alura/adopet/api/model/Tutor.java
@@ -1,5 +1,7 @@
 package br.com.alura.adopet.api.model;
 
+import br.com.alura.adopet.api.dto.AtualizarTutorDto;
+import br.com.alura.adopet.api.dto.CadastroTutorDto;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
@@ -17,24 +19,25 @@ public class Tutor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-
-    @NotBlank
-    @Column(name = "nome")
     private String nome;
-
-    @NotBlank
-    @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
-    @Column(name = "telefone")
     private String telefone;
-
-    @NotBlank
-    @Email
-    @Column(name = "email")
     private String email;
-
     @OneToMany(mappedBy = "tutor")
-    @JsonManagedReference("tutor_adocoes")
     private List<Adocao> adocoes;
+
+    public Tutor(){}
+
+    public Tutor(CadastroTutorDto dto) {
+        this.nome = dto.nome();
+        this.telefone = dto.telefone();
+        this.email = dto.email();
+    }
+
+    public void atulizarDados(AtualizarTutorDto dto) {
+        this.nome = dto.nome();
+        this.telefone = dto.telefone();
+        this.email = dto.email();
+    }
 
     @Override
     public boolean equals(Object o) {
