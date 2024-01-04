ecba7d1265d62ada18030ab3764dcb6214c5f8e4
Wasse Lucas <wasselucas.silva@gmail.com>
2024-01-04 18:49:29 -0300

    criando record AbrigoDTO

--- src/main/java/br/com/alura/adopet/api/dto/CadastroAbrigoDto.java
+++ src/main/java/br/com/alura/adopet/api/dto/CadastroAbrigoDto.java
@@ -1,4 +1,14 @@
 package br.com.alura.adopet.api.dto;
 
-public record CadastroObrigarioDto() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record CadastroAbrigoDto(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
+        String telefone,
+        @NotBlank
+        String email
+) {}
