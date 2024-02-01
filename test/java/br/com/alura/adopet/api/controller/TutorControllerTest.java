387f594a75ca876aa3a031d37b9a3148f6e4b0fe
wasse lucas <wasselucas.silva@gmail.com>
2024-02-01 00:41:54 -0300

    criado teste para devolver codigo 400 para dados invalidos

--- test/java/br/com/alura/adopet/api/controller/TutorControllerTest.java
+++ test/java/br/com/alura/adopet/api/controller/TutorControllerTest.java
@@ -41,4 +41,22 @@ class TutorControllerTest {
         assertEquals(200, response.getStatus());
     }
 
+    @Test
+    void deveriaDevolverCodigo400ParaRequisicaoCadastrarTutorDadosInvalidos() throws Exception {
+        //Arrange
+        String json = """
+                {
+                    "nome": "Rodrigo",
+                    "telefone": "(21)0000-90900",
+                    "email":"email@example.com.br"
+                }
+                """;
+        MockHttpServletResponse response = mockMvc.perform(
+                post("/tutores")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+        assertEquals(400, response.getStatus());
+    }
+
 }
